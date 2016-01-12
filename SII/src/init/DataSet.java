3716dafe44495e2dc9261e554c31a20f78ebeb25
DiegoMariottini <diegomariottini90@gmail.com>
2016-01-12 15:40:17 +0100

    Prova conflitto

--- SII/src/init/DataSet.java
+++ SII/src/init/DataSet.java
@@ -8,4 +8,5 @@ import tokenization.TagParser;
 public class DataSet {
 	private static String cv="PAOLO ROSSI Residenza: Via dei Ciclamini, 4 Cap. 60500 Citt� Pescara Prov. PE Tel. ..085/22222Domicilio: Via .delle Rose, 15..Cap. .20100. Citt� Milano.. Prov. .MI. Tel. .02/333333.Cell. �335/555555e-mail info@cvlavoro.comNato a Pescara. il 26/6/1981. Nazionalit� Italiana. Stato civile celibePatente di guida B. Servizio civile volontario svolto nel 2004 presso il Comune di Milano, area servizi sociali per i minori.";
 	
+	//pippo
 }
