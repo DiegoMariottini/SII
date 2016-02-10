99efaadd7f109382ac0ce301d69519b4f8fcf1ba
lorella <alenia@alenia-ultrabook>
2016-02-10 11:58:39 +0100

    TagParserTest modificato con aggiunta di due CV

--- SII/src/tokenization/TagParserTest.java
+++ SII/src/tokenization/TagParserTest.java
@@ -7,9 +7,11 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public class TagParserTest {
 
 	public static void main(String[] args) throws UnirestException {
-		String prova = "PAOLO ROSSI Residenza: Via dei Ciclamini, 4 Cap. 60500 Citt� Pescara Prov. PE Tel. ..085/22222Domicilio: Via .delle Rose, 15..Cap. .20100. Citt� Milano.. Prov. .MI. Tel. .02/333333.Cell. 335/555555e-mail info@cvlavoro.comNato a Pescara. il 26/6/1981. Nazionalit� Italiana. Stato civile celibe. Patente di guida B. Servizio civile volontario svolto nel 2004 presso il Comune di Milano, area servizi sociali per i minori.";
+		String prova1 = "PAOLO ROSSI Residenza: Via dei Ciclamini, 4 Cap. 60500 Citt� Pescara Prov. PE Tel. ..085/22222Domicilio: Via .delle Rose, 15..Cap. .20100. Citt� Milano.. Prov. .MI. Tel. .02/333333.Cell. 335/555555e-mail info@cvlavoro.comNato a Pescara. il 26/6/1981. Nazionalit� Italiana. Stato civile celibe. Patente di guida B. Servizio civile volontario svolto nel 2004 presso il Comune di Milano, area servizi sociali per i minori.";
+		String prova2 = "Andreoli Enterprise direttore";
 		TagParser parser = new TagParser();
-		parser.parseCV(prova);
+		parser.parseCV(prova1);
+		parser.parseCV(prova2);
 		List<DocParser> lista = new LinkedList<DocParser>();
 		lista = parser.parseQuery("Pescara");
 		for(DocParser dp : lista){
